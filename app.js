f52dcc216431aeeadc043c1c0eaa65fa8c0ea838
Reed Nelson-Saunders <reedns@hotmail.com>
2014-10-25 15:06:22 -0700

    chapter 2

--- app.js
+++ app.js
@@ -5,18 +5,52 @@ function() {
     this.products = gems;
   });
 
+  app.controller('TabController', function() {
+    this.tab = 1;
+
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+
+    this.isSet = function(checkTab) {
+      return this.tab === checkTab;
+    };
+  });
+
+  app.controller('GalleryController', function(){
+    this.current = 0;
+    this.setCurrent = function(newGallery){
+      this.current = newGallery || 0;
+    };
+  });
+
   var gems = [
   {
     name: 'Azurite',
-    price: 2.95,
-    canPurchase: true,
-    soldOut: false
-  },
-  {
-    name:'Zircon'
-    price: 3.95,
-    canPurchase: true,
-    soldOut: false
-    }
+    description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+    shine: 8,
+    price: 110.50,
+    rarity: 7,
+    color: '#CCC',
+    faces: 14,
+    images: [
+      "images/gem-02.gif",
+      "images/gem-05.gif",
+      "images/gem-09.gif"
+    ]
+  }, {
+   name: 'Zircon',
+    description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+    shine: 70,
+    price: 1100,
+    rarity: 2,
+    color: '#000',
+    faces: 6,
+    images: [
+      "images/gem-06.gif",
+      "images/gem-07.gif",
+      "images/gem-10.gif"
+    ]
+  }
     ];
 }
